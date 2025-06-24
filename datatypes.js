c520c84e7db0b4f410cb8c144de0d1a002f3de55
sabhya1290 <sabhya1290@gmail.com>
2025-06-24 11:44:23 +0530

    DataTypes Done

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using node.js not browser
+
+//number => 2 to power 52
+//bigint
+//string => ""
+//boolean =>True or False
+//null
+//undefined
+//symbol
+
+//object
+
+console.log(typeof 3); //number
+console.log(typeof 3.14); //number  
+console.log(typeof "3"); //string
+console.log(typeof true); //boolean
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+console.log(typeof Symbol("hello")); //symbol   
+console.log(typeof {}); //object
