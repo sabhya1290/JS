14343651af97f34a619808079933aa5fc801c6fb
sabhya1290 <sabhya1290@gmail.com>
2025-06-26 12:10:09 +0530

    conversion of datatype

---
+++ 03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "3aabd" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// ""  =>  false
+// "name" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
